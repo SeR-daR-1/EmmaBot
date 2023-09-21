c445e7fe0466ff23a47b1616afe0574bc146e4a0
AHMED/@bbnnQ <143324973+AhmedXMusic@users.noreply.github.com>
2023-09-21 23:21:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AhmedXMusic/v3",
+    "https://github.com/AhmedXMusic/pythonnew",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
