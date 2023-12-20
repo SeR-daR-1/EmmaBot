b4a2c7e660ec987957dcd5775a5500127ca440d7
Ema-02 <152199222+Ema-02@users.noreply.github.com>
2023-12-20 18:46:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/x-bot-emma/EmmaV3",
+    "https://github.com/Ema-02/Remix",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
