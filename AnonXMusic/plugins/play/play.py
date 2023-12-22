9175aba85ea4e23f307566f49ebc2469b2c6d7f1
Ema-02 <152199222+Ema-02@users.noreply.github.com>
2023-12-23 02:13:59 +0300

    Update play.py

--- AnonXMusic/plugins/play/play.py
+++ AnonXMusic/plugins/play/play.py
@@ -41,7 +41,7 @@ async def check_is_joined(message):
         status = await app.get_chat_member("cczza", userid)
         return True
     except Exception:
-        await message.reply_text(f'🚧┇عزيزي: {message.from_user.mention}\n🚧┇أشتࢪك في قناة البوت أولاً.\n🚧┇قناة البوت: @cczza 🧚‍♀',reply_markup=force_btn,disable_web_page_preview=False)
+        await message.reply_text(f'- عزيزي: {message.from_user.mention}\n- أشتࢪك في قناة البوت أولاً.\n- قناة البوت: @cczza 🧚‍♀',reply_markup=force_btn,disable_web_page_preview=False)
         return False
 
 
