c278750f713f1ad2228eb2aa4111bc60a1ef87f5
Abdulhadi <129096669+coowl@users.noreply.github.com>
2024-01-13 20:34:43 +0400

    Update help.py

--- AnonXMusic/utils/inline/help.py
+++ AnonXMusic/utils/inline/help.py
@@ -21,43 +21,11 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_1"],
                     callback_data="help_callback hb1",
                 ),
+            ],
+            [
                 InlineKeyboardButton(
                     text=_["H_B_2"],
-                    callback_data="help_callback hb2",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_3"],
-                    callback_data="help_callback hb3",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_7"],
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_9"],
-                    callback_data="help_callback hb9",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_11"],
-                    callback_data="help_callback hb11",
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text="الأوامر العرببة",
-                    callback_data="help_callback hb15",
+                    callback_data="help_callback hb2",                                
                 ),
             ],
             mark,
