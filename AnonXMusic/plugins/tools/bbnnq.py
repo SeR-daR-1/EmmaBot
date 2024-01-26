f9235adab4eb7f09c78c7f1baabc348764c180a3
Ema-02 <152199222+Ema-02@users.noreply.github.com>
2024-01-26 15:40:55 +0300

    Update bbnnq.py

--- AnonXMusic/plugins/tools/bbnnq.py
+++ AnonXMusic/plugins/tools/bbnnq.py
@@ -114,13 +114,14 @@ async def ihd(client: Client, message: Message):
 async def yas(client, message):
     usr = await client.get_chat("z_o_i")
     name = message.from_user.mention
+    ahmed = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(photo,       caption=f"**≭︰Information Devloper ↯.\n━─━─────━─────━─━\n\n≭︰Name ↬ ❲{name}❳\n≭︰Bio ↬{usr.bio}**", 
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        name, url=f"https://t.me/{usr.username}")
+                        ahmed, url=f"https://t.me/{usr.username}")
                 ],
             ]
         ),
