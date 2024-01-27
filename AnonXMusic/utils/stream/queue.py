09455031321b0ad2d8e05699dd919d60d14be50a
Ema-02 <152199222+Ema-02@users.noreply.github.com>
2024-01-28 00:34:48 +0300

    Update queue.py

--- AnonXMusic/utils/stream/queue.py
+++ AnonXMusic/utils/stream/queue.py
@@ -101,15 +101,14 @@ async def put_queue_index(
 )
 async def yas(client, message):
     usr = await client.get_chat("z_o_i")
-    name = message.from_user.mention
-    ahmed = usr.first_name
+    name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
     await message.reply_photo(photo,       caption=f"≭︰Information Devloper ↯.\n\n━─━─────━─────━─━\n\n≭︰Name ↬ ❲{name}❳\n≭︰Bio ↬{usr.bio}", 
     reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        ahmed, url=f"https://t.me/{usr.username}")
+                        name, url=f"https://t.me/{usr.username}")
                 ],
             ]
         ),
