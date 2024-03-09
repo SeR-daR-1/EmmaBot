92aaf9003777c6b65f4da6e9faba74f4766dd47e
SeRdaR <160137007+SeR-daR-1@users.noreply.github.com>
2024-03-10 00:02:09 +0300

    Update must_join.py

--- AnonXMusic/plugins/tools/must_join.py
+++ AnonXMusic/plugins/tools/must_join.py
@@ -5,20 +5,20 @@ from AnonXMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/cczza":  # Not compulsory
+    if not "https://t.me/+Dq8oYGQ7BBEzMjFk":  # Not compulsory
         return
     try:
         try:
             await bot.get_chat_member("cczza", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/cczza".isalpha():
+            if "https://t.me/+Dq8oYGQ7BBEzMjFk".isalpha():
                 link = "https://t.me/cczza"
             else:
-                chat_info = await bot.get_chat("cczza")
+                chat_info = await bot.get_chat("https://t.me/+Dq8oYGQ7BBEzMjFk")
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @cczza .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ!",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("فَلسفة مشاعر .", url=link)]
