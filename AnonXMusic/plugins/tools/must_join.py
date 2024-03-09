9cb0c8b79c530fa325e1779e9028c34a10460462
SeRdaR <160137007+SeR-daR-1@users.noreply.github.com>
2024-03-10 00:19:15 +0300

    Update must_join.py

--- AnonXMusic/plugins/tools/must_join.py
+++ AnonXMusic/plugins/tools/must_join.py
@@ -5,25 +5,25 @@ from AnonXMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/+Dq8oYGQ7BBEzMjFk":  # Not compulsory
+    if not "https://t.me/zc_cw":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("https://t.me/+Dq8oYGQ7BBEzMjFk", msg.from_user.id)
+            await bot.get_chat_member("zc_cw", msg.from_user.id)
         except UserNotParticipant:
-            if "https://t.me/+Dq8oYGQ7BBEzMjFk".isalpha():
-                link = "https://t.me/+Dq8oYGQ7BBEzMjFk"
+            if "https://t.me/zc_cw".isalpha():
+                link = "https://t.me/zc_cw"
             else:
-                chat_info = await bot.get_chat("https://t.me/+Dq8oYGQ7BBEzMjFk")
+                chat_info = await bot.get_chat("zc_cw")
                 link = chat_info.invite_link
             user_channel_info = await bot.get_chat(msg.from_user.id)
             user_channel_invite_link = await bot.export_chat_invite_link(user_channel_info.id)
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ! \n⌯︙قم بالانضمام إلى قناتك الخاصة من هنا: {user_channel_invite_link}",
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ! \n⌯︙قم بالانضمام إلى القناة من هنا: {user_channel_invite_link}",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("فَلسفة مشاعر .", url=link)]
+                        [InlineKeyboardButton("!", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
