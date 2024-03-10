383be045b1b13846c19fb1ebd068244782e386e4
SeRdaR <160137007+SeR-daR-1@users.noreply.github.com>
2024-03-10 08:57:04 +0300

    Update must_join.py

--- AnonXMusic/plugins/tools/must_join.py
+++ AnonXMusic/plugins/tools/must_join.py
@@ -3,6 +3,10 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from AnonXMusic import app
 
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant, ChatWriteForbidden, ChatAdminRequired
+
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
     if not "https://t.me/zc_cw":  # Not compulsory
@@ -10,7 +14,9 @@ async def must_join_channel(bot: Client, msg: Message):
     try:
         try:
             await bot.get_chat_member("zc_cw", msg.from_user.id)
+            verification_status = "تم التحقق من الاشتراك في القناة ✅"
         except UserNotParticipant:
+            verification_status = "لم يتم التحقق من الاشتراك في القناة ❌"
             if "https://t.me/zc_cw".isalpha():
                 link = "https://t.me/zc_cw"
             else:
@@ -18,10 +24,10 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ!",
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙{verification_status}",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("!", url=link)]
+                        [InlineKeyboardButton("التحقق من الاشتراك", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
